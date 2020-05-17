f7a1211db60a712295d76e13545d6966a6a4a674
wangwenhai <751957846@qq.com>
2020-05-17 20:25:22 +0800

    :construction: add cluster support

--- src/trump_distribution.erl
+++ src/trump_distribution.erl
@@ -1,3 +1,17 @@
+%% Copyright 2020 wangwenhai
+%% 
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%% 
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
 %%%-------------------------------------------------------------------
 %%% @author wwhai
 %%% 分布层，主要用来实现分布式集群
@@ -12,6 +26,7 @@
 -export([broad_cast_local/0]).
 -export([get_nodes/0]).
 -export([get_clients/0]).
+-export([add_node/1]).
 %%
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
@@ -24,28 +39,29 @@
 %% Behavioural functions
 %% ====================================================================
 -record(state, {}).
-%% 节点属性
--record(trump_cluster_node, {name}).
 %% 集群状态下的客户端，多了一个Node属性
--record(trump_cluster_client, {node, client_id, ip, auth, transport, socket}).
+-record(trump_cluster_client, {trump_cluster_node, client_id}).
+%% 节点保存表
 -define(CLUSTER_NODE_TABLE,trump_cluster_node_table).
+%% 集群的客户端和节点之间的关系表
 -define(CLUSTER_CLIENT_TABLE,trump_cluster_client_table).
+%% 单节点的客户端表
 -define(TRUMP_CLIENT_TABLE, trump_client_table).
 
 %% 
 %% start_link/0
 %% ====================================================================
 start() ->
-    %% 集群内的客户端存放表
-    EtsOptionsTcci = [named_table, public, set, {keypos, #trump_cluster_client.client_id}, {write_concurrency, true}, {read_concurrency, true}],
-    ets:new(?CLUSTER_CLIENT_TABLE, EtsOptionsTcci),
-    %% 集群内的节点存放表
-    EtsOptionsTccn = [named_table, public, set, {keypos, #trump_cluster_node.name}, {write_concurrency, true}, {read_concurrency, true}],
-    ets:new(?CLUSTER_NODE_TABLE, EtsOptionsTccn),
+    mnesia:delete_schema(get_nodes()),
+    mnesia:create_schema(get_nodes()),
+    mnesia:start(),                                      
+    %% 集群内的客户端存放表                                          
+    mnesia:create_table(?CLUSTER_CLIENT_TABLE, [{ram_copies, get_nodes()},  
+                                              {attributes, record_info(fields, trump_cluster_client)}]),  
+    mnesia:add_table_index(?CLUSTER_CLIENT_TABLE, pid),                                        
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 %% init/1
-%%  ets:match_object(trump_cluster_node , {trump_cluster_node , '$1' })
 init([]) ->
     {ok, #state{}}.
 
@@ -88,23 +104,28 @@ code_change(_OldVsn, State, _Extra) ->
 %% Start distribution layer
 %%
 get_nodes()->
-    {ok,{{mode,Mode,cluster_nodes,  Nodes}}} = application:get_env(trump, cluster),
-    case Mode of
-        static ->
-            Nodes;
-        discovery ->
-            %% 从ETS表中读取,后期实现
-            ets:match_object(?CLUSTER_NODE_TABLE , {trump_cluster_node , '$1'})
-    end.
+    [node() | nodes()].
+    % {ok,{{mode,Mode,cluster_nodes,  Nodes}}} = application:get_env(trump, cluster),
+    % case Mode of
+    %     static ->
+    %         Nodes;
+    %     discovery ->
+    %         [node() | nodes()]
+    % end.
 
 %%
 %% 动态添加一个节点
 %%
-add_node(NodeName)->
+add_node(Node)->
     %% 首先判断是否ping的到
     %% if ping -> insert
     %% else -> log
-    ets:insert(?CLUSTER_NODE_TABLE , #trump_cluster_node{name =NodeName}).
+    case net_adm:ping(Node) of 
+        pong ->
+            io:format("Distribution LOG =*-*-*-*-*-=> Node :~p join cluster success!Cluster nodes is~p:~n",[Node,get_nodes()]);
+        pang ->
+            io:format("Distribution LOG =*-*-*-*-*-=> Node :~p join cluster failed!~n",[Node])
+        end.
 %%
 %% ets:match_object(trump_connection_table , {trump_client_info , '$1', '$2' , '$3', '$4' , '$5' })
 %%
@@ -121,7 +142,3 @@ synchroized_cluster()->
 %%
 broad_cast_local()->
     ok.
-
-%%
-%% 获取本节点上面的所有客户端
-%%
